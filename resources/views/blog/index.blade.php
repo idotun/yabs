3d659f377f3b2278eb6c8227594895f6e53693de
Dotun Olukoya <dhotun@yahoo.com>
2017-02-27 23:20:32 +0000

    First Commit

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,239 @@
+    @extends ('layouts.main')
+
+    @section('content')    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <article class="post-item">
+                   
+                    <div class="post-item-image">
+                        <a href="post.html">
+                            <img src="img/igala.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="post-item-body">
+                        <div class="padding-10">
+                           <p><b><a href="post.html">Igala that befits a Miss Igala</a></b></p>
+                           
+                        </div>
+                    </div>
+                    
+                </article>
+            </div>
+            <div class="col-md-3">
+                <article class="post-item">
+                    <div class="post-item-image">
+                        <a href="post.html">
+                            <img src="img/igbo_thumb.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="post-item-body">
+                        <div class="padding-10">
+                            <p><b><a href="post.html">Lorem ipsum dolor sit amet, consectetur</a></b></p>
+                           
+                        </div>
+                    </div>
+                </article>
+             </div>
+             <div class="col-md-3">
+                <article class="post-item">
+                    <div class="post-item-image">
+                        <a href="post.html">
+                            <img class="img-responsive" src="img/nokia_thumb.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="post-item-body">
+                        <div class="padding-10">
+                           <p><b><a href="post.html">Lorem ipsum dolor sit amet, consectetur</a></b></p>
+                          
+                        </div>
+
+                    </div>
+                </article>
+                 </div>
+                <div class="col-md-3">
+                <article class="post-item">
+                   
+                    <div class="post-item-image">
+                        <a href="post.html">
+                            <img src="img/igala.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="post-item-body">
+                        <div class="padding-10">
+                            <p><b><a href="post.html">Lorem ipsum dolor sit amet, consectetur</a></b></p>
+                           
+                        </div>
+                    </div>
+                    
+                </article>
+            </div>
+
+<!--
+                <nav>
+                  <ul class="pager">
+                    <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Newer</a></li>
+                    <li class="next"><a href="#">Older <span aria-hidden="true">&rarr;</span></a></li>
+                  </ul>
+                </nav>
+-->
+           
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <article class="post-item">
+                   
+                    <div class="post-item-image">
+                        <a href="post.html">
+                            <img src="img/igala.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="post-item-body">
+                        <div class="padding-10">
+                           <p><b><a href="post.html">Lorem ipsum dolor sit ame, consectetur</a></b></p>
+                           
+                        </div>
+                    </div>
+                    
+                </article>
+            </div>
+            <div class="col-md-3">
+                <article class="post-item">
+                    <div class="post-item-image">
+                        <a href="post.html">
+                            <img src="img/igbo_thumb.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="post-item-body">
+                        <div class="padding-10">
+                            <p><b><a href="post.html">Lorem ipsum dolor sit amet, consectetur</a></b></p>
+                           
+                        </div>
+                    </div>
+                </article>
+             </div>
+             <div class="col-md-3">
+                <article class="post-item">
+                    <div class="post-item-image">
+                        <a href="post.html">
+                            <img class="img-responsive" src="img/nokia_thumb.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="post-item-body">
+                        <div class="padding-10">
+                           <p><b><a href="post.html">Lorem ipsum dolor sit amet, consectetur</a></b></p>
+                          
+                        </div>
+
+                    </div>
+                </article>
+                 </div>
+                <div class="col-md-3">
+                <article class="post-item">
+                   
+                    <div class="post-item-image">
+                        <a href="post.html">
+                            <img src="img/igala.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="post-item-body">
+                        <div class="padding-10">
+                            <p><b><a href="post.html">Lorem ipsum dolor sit amet, consectetur</a></b></p>
+                           
+                        </div>
+                    </div>
+                    
+                </article>
+            </div>
+
+<!--
+                <nav>
+                  <ul class="pager">
+                    <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Newer</a></li>
+                    <li class="next"><a href="#">Older <span aria-hidden="true">&rarr;</span></a></li>
+                  </ul>
+                </nav>
+-->
+           
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <article class="post-item">
+                   
+                    <div class="post-item-image">
+                        <a href="post.html">
+                            <img src="img/igala.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="post-item-body">
+                        <div class="padding-10">
+                           <p><b><a href="post.html">Lorem ipsum dolor sit ame, consectetur</a></b></p>
+                           
+                        </div>
+                    </div>
+                    
+                </article>
+            </div>
+            <div class="col-md-3">
+                <article class="post-item">
+                    <div class="post-item-image">
+                        <a href="post.html">
+                            <img src="img/igbo_thumb.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="post-item-body">
+                        <div class="padding-10">
+                            <p><b><a href="post.html">Lorem ipsum dolor sit amet, consectetur</a></b></p>
+                           
+                        </div>
+                    </div>
+                </article>
+             </div>
+             <div class="col-md-3">
+                <article class="post-item">
+                    <div class="post-item-image">
+                        <a href="post.html">
+                            <img class="img-responsive" src="img/nokia_thumb.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="post-item-body">
+                        <div class="padding-10">
+                           <p><b><a href="post.html">Lorem ipsum dolor sit amet, consectetur</a></b></p>
+                          
+                        </div>
+
+                    </div>
+                </article>
+                 </div>
+                <div class="col-md-3">
+                <article class="post-item">
+                   
+                    <div class="post-item-image">
+                        <a href="post.html">
+                            <img src="img/igala.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="post-item-body">
+                        <div class="padding-10">
+                            <p><b><a href="post.html">Lorem ipsum dolor sit amet, consectetur</a></b></p>
+                           
+                        </div>
+                    </div>
+                    
+                </article>
+            </div>
+
+<!--
+                <nav>
+                  <ul class="pager">
+                    <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Newer</a></li>
+                    <li class="next"><a href="#">Older <span aria-hidden="true">&rarr;</span></a></li>
+                  </ul>
+                </nav>
+-->
+           
+        </div>
+    </div>
+    @endsection
+
+    

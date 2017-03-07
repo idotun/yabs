8c4083fbc9fd5d435f08d2c15af1241f573787ee
Dotun Olukoya <dhotun@yahoo.com>
2017-03-07 17:26:46 +0000

    fix social sharing css

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -22,16 +22,14 @@
                     
 
 
-            <div class="container  post-meta padding-10 clearfix">
-               <div class="row">
-                   <div class="col-md-6">
+            <div class="post-meta padding-10 clearfix">
+        
+                   <div class="facebook-share">
                         <a class="btn btn-primary btn-lg" href="javascript:fbShare('http://justincron.com', 'Fb Share', 'Facebook share popup', '', 520, 350)"><i class="fa fa-facebook fa-lg"></i> Share on Facebook</a>
                    </div>
-                   <div class="col-md-6">
+                   <div class="twitter-share">
                          <a class="btn btn-info btn-lg tweet" title="Justin Cron" href="http://justincron.com" target="_blank"><i class="fa fa-twitter fa-lg"></i> Share on Twitter</a>
                    </div>
-               </div>
-           
             </div>
              </article>
 <!--comments-->

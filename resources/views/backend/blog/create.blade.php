846464e0e75e0e38d7e9100a3889c72518366f13
Dotun Olukoya <dhotun@yahoo.com>
2017-03-30 07:33:49 +0100

    add simplemde to body form

--- resources/views/backend/blog/create.blade.php
+++ resources/views/backend/blog/create.blade.php
@@ -71,5 +71,8 @@
 @section('script')
     <script type="text/javascript">
         $('ul.pagination').addClass('no-margin pagination-sm');
+        
+        var simplemde = new SimpleMDE({ element: $("#body")[0] });
+     
     </script>
 @endsection
